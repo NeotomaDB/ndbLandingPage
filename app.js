d891b85ebff31e00170367f208be6dfdb88e4a68
Michael Stryker <michael.s.stryker@gmail.com>
2018-11-20 15:59:45 -0500

    set port to match proxy

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs')
+const httpPort = 3002
+
+http.createServer((req, res) => {
+  fs.readFile('index.htm', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.htm" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
+}).listen(httpPort, () => {
+  console.log('Server listening on: http://localhost:%s', httpPort)
+})

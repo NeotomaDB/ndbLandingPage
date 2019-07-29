2bf81e706e8e728480639a9dad6ef2636318ade2
Simon <simon.j.goring@gmail.com>
2019-07-29 05:07:36 -0700

    Added a data formatting fix to render related datasets. As in issue #6, re-formatting of the site/dataset pathway changed how related datasets were rendered.  This has been fixed now.

--- src/components/commondata.vue
+++ src/components/commondata.vue
@@ -4,7 +4,6 @@
     <h2>Other Associated Datasets</h2>
 
     <h3>Datasets at the Same Site</h3>
-
     <div v-if="this.dataset.length > 0" class="cardbox">
       <div v-for="i in this.dataset"  class="namecard">
         <small>[<strong><a target="_blank" :href= "i.datasetid">{{ i.datasetid }}</a></strong>]</small> {{ i.datasettype.charAt(0).toUpperCase() + i.datasettype.slice(1) }}<br>
@@ -49,37 +48,18 @@ export default {
             .then((response) => { return response.json() })
             .then((data) => {
               var dsdt = data.data
-              self.dataset = dsdt[0].site.datasets.map( function(y)
+
+              self.dataset = dsdt.map( function(y)
                   {
-                    return {datasetid: y.datasetid,
-                            datasettype: y.datasettype,
-                            database: y.database}
+                    return {datasetid: y.site.datasets[0].datasetid,
+                            datasettype: y.site.datasets[0].datasettype,
+                            database: y.site.datasets[0].database}
                   })
               .flat()
               .filter(x => x.datasetid !== parseInt(this.dsid))
 
             })
       })
-
-      /*fetch('http://api-dev.neotomadb.org/v2.0/data/datasets/' + this.dsid + '/publications')
-        .then((response) => { return response.json() })
-        .then((data) => {
-          /* Modifying the values and processing the inputs
-          var pid = data.data.map(x => x.publicationid)
-
-          pid.map(x =>
-            fetch('http://api-dev.neotomadb.org/v2.0/data/sites/' + x + '/datasets')
-            .then((response) => { return response.json() })
-            .then((data) => {
-              var outsets =  data.data[0].dataset.filter( x=> !(x.datasetid === parseInt(this.dsid) ))
-              if (outsets === null) {
-                self.pbndataset = '';
-              } else {
-                self.pbndataset = outsets.filter((v, i, a) => a.map(x => x.datasetid).indexOf(v.datasetid) === i);
-              }
-            })
-      }) */
-
     }
   },
   mounted() {

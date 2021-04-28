084fd6a2de941cc2edf184ac22c5444f81a2a507
Jonathan Nelson <jkn128@psu.edu>
2021-04-28 16:02:39 -0400

    enable GA for landing pages

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueGtag from 'vue-gtag';
 // import BootstrapVue from 'bootstrap-vue'
 
 import App from './App.vue'
-import "leaflet/dist/leaflet.css";
+import 'leaflet/dist/leaflet.css';
 
 import { BContainer, BCol, BRow, BCard, BTooltip } from 'bootstrap-vue';
 Vue.component('b-container', BContainer);
@@ -18,15 +19,15 @@ Vue.component('l-tile-layer', LTileLayer);
 Vue.component('l-marker', LMarker);
 
 // eslint-disable-next-line
-delete L.Icon.Default.prototype._getIconUrl
+delete L.Icon.Default.prototype._getIconUrl;
 // eslint-disable-next-line
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-})
+});
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const router = new VueRouter({
   mode: 'history',
@@ -37,24 +38,27 @@ const router = new VueRouter({
       name: 'root'
     },
     {
-      path: "/:dsid/",
+      path: '/:dsid/',
       component: App,
       props: true
     },
     {
-      path: "/datasets/:dsid/",
-      alias: "/:dsid",
+      path: '/datasets/:dsid/',
+      alias: '/:dsid',
       component: App,
       props: true
     }
   ]
-})
+});
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+Vue.use(VueGtag, {
+  config: { id: 'G-3XDNWKENJ4' }
+}, router);
 // Vue.use(BootstrapVue)
 
 new Vue({
   el: '#app',
   router: router,
   render: h => h('router-view')
-})
+});
